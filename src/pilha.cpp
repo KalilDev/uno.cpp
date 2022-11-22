714a7f610b310b5d0fc1d9476a458e7ae9f5645b
Maria Eduarda <dudah.s3@gmail.com>
2022-11-22 20:57:49 -0300

    Pilha: implementando função de preencher o vetor de cartas

--- src/pilha.cpp
+++ src/pilha.cpp
@@ -1,5 +1,52 @@
-/**
- * @file pilha.cpp
- * O arquivo que implementa a classe Pilha
- */
 #include "pilha.hpp"
+#include "carta_especial.hpp"
+
+Pilha::Pilha(){
+
+}
+
+Pilha::Pilha(Carta* carta){
+
+}
+
+Pilha::~Pilha(){
+    for (auto carta: _cartas)
+        delete carta;
+}
+
+Carta* Pilha::getTop(){
+    Carta* ultima = _cartas[_cartas.size() - 1];
+    return ultima;
+}
+
+Carta* Pilha::pop(){
+    Carta *ultima = _cartas[_cartas.size() - 1];
+    _cartas.pop_back();
+    return ultima;
+}
+
+void Pilha::push(Carta* carta){
+    _cartas.push_back(carta);
+}
+
+void Pilha::random(){
+
+}
+
+Pilha Pilha::cheia(){
+    Pilha pilha;
+    for (int c = 0; c < 4; c++){
+        CorDaCarta cor = static_cast<CorDaCarta>(c);
+        for (int i = 0; i < 10; i++)
+            pilha.push(new Carta(cor, i));
+        
+        pilha.push(new CartaEspecial(cor, TipoDeCartaEspecial::Bloqueia));
+        pilha.push(new CartaEspecial(cor, TipoDeCartaEspecial::ComeDois));
+        pilha.push(new CartaEspecial(cor, TipoDeCartaEspecial::Reverso));
+    }
+}
+
+size_t Pilha::size(){
+ return _cartas.size();
+}
+
