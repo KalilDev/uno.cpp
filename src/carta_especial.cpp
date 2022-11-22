078774a90a72c311bc9a81fa58cb25a6c697e23c
Gustavo <gstavomota1@gmail.com>
2022-11-22 19:59:11 -0300

    Carta/CartaEspecial: Implementando funções

--- src/carta_especial.cpp
+++ src/carta_especial.cpp
@@ -3,3 +3,12 @@
  * O arquivo que implementa a classe CartaEspecial
  */
 #include "carta_especial.hpp"
+
+CartaEspecial::CartaEspecial(CorDaCarta cor, TipoDeCartaEspecial tipo) : Carta::Carta(cor,static_cast<numero_da_carta>(tipo)) {
+	_tipo = tipo;
+}
+
+TipoDeCartaEspecial CartaEspecial::getTipo() {
+	return _tipo;
+}
+
