a8bd162ff47a06791b84cfcef35412a48b06c842
Bruno Lima <brunolima280103@gmail.com>
2022-11-22 20:44:54 -0300

    Mao : Subindo documentação e código inicial

--- include/mao.hpp
+++ include/mao.hpp
@@ -1,7 +1,21 @@
 /**
  * @file mao.hpp
+ * @author Bruno Lima Soares (brunolima280103@gmail.com)
+ * @brief Classe responsável pela manipulação das cartas de um jogador em uma rodada
+ * @version 0.1
+ * @date 2022-11-22
+ * @details Classe Mão, que implementa métodos para manipular as cartas que estão na
+ * "mão" de um jogador em uma rodada, podendo acrescentar novas cartas, remover outras.
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * @file mao.hpp
  * O arquivo que descreve a classe Mao
  */
+
 #pragma once
 #include <cstdlib>
 #include <vector>
@@ -9,18 +23,72 @@
 
 /** foo
  */
+
+/**
+ * @brief Classe que faz a manipulação das cartas de um usuário em uma rodada
+ * 
+ * @details Permite o fluxo do jogo e das jogadas, adicionando e removendo cartas 
+ * 
+ */
+
 class Mao {
+
 private:
+
     std::vector<Carta *> _cartas;
+
 public:
-    Mao();
-    size_t size();
-    Carta *begin();
-    Carta *end();
-    void adicionarCarta(Carta *);
-    Carta *removerCarta(size_t);
-    CorDaCarta getCorDaCarta(size_t);
-    Carta *operator[](size_t);
+    
+    /**
+     * @brief Construtor de um novo objeto mão
+     * @details Construtor sem parâmetros para um novo objeto mão
+     */
+        Mao();
+
+    /**
+     * @brief Função que retorna um ponteiro para o início do vector
+     * @details Função que pega o ponteiro para o início do vector, desreferencia e faz seu retorno
+     * @return Carta* 
+     */
+        Carta *begin();
+
+    /**
+     * @brief Função que retorna um ponteiro para o final do vector
+     * @details Função que pega o ponteiro para o fim do vector, desreferencia e faz seu retorno
+     * @return Carta* 
+     */
+        Carta *end();
+
+    /**
+     * @brief Função responsável por adicionar uma carta à mão
+     * @details Função que recebe uma carta como parâmetro e a insere na última posição do vector que armazena cartas na classe Mão
+     * @return * void 
+     */
+        void adicionarCarta(Carta *);
+
+    /**
+     * @brief Função responsável por remover uma carta da mão
+     * @details Função que recebe como parâmetro o número de uma carta e remove da mão a carta de número correspondente
+     * @return * Carta* 
+     */
+        Carta *removerCarta(size_t);
+
+    /**
+     * @brief Função get que retorna a cor da carta
+     * @details Função que retorna a cor de uma carta presente na mão
+     * @return CorDaCarta 
+     */
+        CorDaCarta getCorDaCarta(size_t);
+
+    /**
+     * @brief 
+     * 
+     * @return Carta* 
+     */
+        Carta *operator[](size_t);
+    
+        size_t size();
+
 };
 
 extern "C" {
--- src/mao.cpp
+++ src/mao.cpp
@@ -2,4 +2,66 @@
  * @file mao.cpp
  * O arquivo que implementa a classe Mao
  */
+
 #include "mao.hpp"
+
+    size_t size(){
+        return sizeof(size_t);
+    }
+
+    Carta Mao::*begin(){
+        *size_t = _cartas.begin();
+        return size_t;
+    }
+
+    Carta Mao::*end(){
+        *size_t = _cartas.end();
+        return size_t;
+    }
+
+    void Mao::adicionarCarta(Carta * c){
+        _cartas.push_back(c);
+    }
+
+    Carta Mao::*removerCarta(size_t){
+        for(auto &x : _cartas){
+            int aux = _cartas.getNumero();
+            if(aux == size_t){
+                _cartas.erase(x);
+            }
+        }
+    }
+
+    CorDaCarta Mao::getCorDaCarta(size_t){
+        return _cartas.getCor().at(size_t);
+    }
+
+    Carta Mao::*operator[](size_t){
+
+    }
+
+extern "C" {
+    size_t mao_size(Mao* self){
+        self->mao_size;
+    }
+
+    Carta *mao_begin(Mao* self){
+        self->mao_begin;
+    }
+
+    Carta *mao_end(Mao* self){
+        self->mao_end;
+    }
+
+    void mao_adicionar_carta(Mao*, Carta*){
+
+    }
+
+    Carta *mao_remover_carta(Mao* self, size_t){
+        self->mao_remover_carta;
+    }
+
+    CorDaCarta mao_get_cor_da_carta(Mao* self, size_t){
+        self->mao_get_cor_da_carta;
+    }
+}
