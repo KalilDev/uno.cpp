ad842484c1c208e5b99dfd5c59ea19c526f7e44b
Raissa <95766853+raissagd@users.noreply.github.com>
2022-11-22 19:59:04 -0300

    interface: documentar e implementar regras

--- include/interface.hpp
+++ include/interface.hpp
@@ -1,31 +1,80 @@
 /**
- * @file interface.hpp
- * O arquivo que descreve a classe Interface
+ * @file interface.cpp
+ * @author Raissa Diniz (raissagdiniz@gmail.com)
+ * @brief Classe responsável pela interface/ menu do jogo
+ * @version 1.0
+ * @date 2022-11-22
+ * @details A classe interface funciona como uma espécie de menu, sendo responsável por disponibilizar
+ * diferentes opções de interação do jogador com o jogo através do terminal, são elas:
+ * começar, reiniciar ou sair do jogo, e ler as instruções;
+ *
+ * @copyright GNU General Public License v2.0
+ *
  */
 #pragma once
 #include "partida.hpp"
 #include <string>
 
-/** foo
+/**
+ * @brief Disponibiliza um pequeno menu ao usuário.
+ *
+ * @details Permite ao usuário escolher quando deseja começar, sair, ou reiniciar a partida, além da opção
+ * de ler as instruções do jogo.
  */
-class Interface {
+class Interface
+{
 private:
+    /**
+     * @brief inicializa um objeto do tipo Partida.
+     *
+     */
     Partida _partida;
+
 public:
+    /**
+     * @brief Constrói um objeto do tipo Interface.
+     *
+     * @details Inicialização sem parâmetros.
+     */
     Interface();
+
+    /**
+     * @brief Retorna um ponteiro para um objeto do tipo Partida.
+     *
+     */
     Partida *getPartida();
+
+    /**
+     * @brief Retorna uma string com todas as instruções específicas ao jogo.
+     *
+     */
     std::string getInstrucoes();
+
+    /**
+     * @brief Fornece ao usuário a opção de desistir da partida e não continuar jogando.
+     *
+     */
     void sair();
+
+    /**
+     * @brief Fornece ao usuário a opção de recomeçar a partida.
+     */
     void resetar();
+
+    /**
+     * @brief Permite ao jogador começar a partida.
+     *
+     */
     void comecar();
 };
 
-extern "C" {
-    Interface* interface_new();
-    void interface_delete(Interface*);
-    Partida *interface_get_partida(Interface*);
-    char *interface_get_instrucoes(Interface*);
-    void interface_sair(Interface*);
-    void interface_resetar(Interface*);
-    void interface_comecar(Interface*);
+extern "C"
+{
+    Interface *interface_new();
+    void interface_delete(Interface *);
+    Partida *interface_get_partida(Interface *);
+    char *interface_get_instrucoes(Interface *);
+    void interface_sair(Interface *);
+    void interface_resetar(Interface *);
+    void interface_comecar(Interface *);
 }
--- src/interface.cpp
+++ src/interface.cpp
@@ -2,4 +2,35 @@
  * @file interface.cpp
  * O arquivo que implementa a classe Interface
  */
+
 #include "interface.hpp"
+
+Interface::Interface();
+
+Partida *Interface::getPartida();
+
+std::string Interface::getInstrucoes()
+{
+    std::string regras = "Regras do nosso (humilde) Uno:
+        1) O jogo consiste em 4 jogadores, com 7 cardas cada;
+        2) Além das clássicas cartas numéricas(0 - 9) de cores variadas(amarelo, azul, vermelho e verde), 
+        essa versão apresenta três tipos de carta especial(em detrimento às cinco do jogo padrão): 
+        - Reverso : muda o sentindo da partida, podendo ser horário ou anti-horário;
+        -Bloqueio : impede o jogador seguinte ao que lançou a carta de participar nessa partida;
+        - “Come 2” : faz com que o próximo àquele que jogou adicione mais 2 cartas à sua mão.É importante ressaltar que, nesse jogo, essas cartas não são acumulativas, ou seja,
+            um outro “come 2” não pode ser jogado em cima de um previamente lançado de modo a “formar um come 4”;
+        3) O jogador não possui a possibilidade de gritar “Uno” enquanto joga sua penúltima carta
+           (uma vez que isso implicaria na necessidade da criação de um sistema de denúncia para quando isso não acontece);
+        4) A interface em que o jogo ocorrerá será o próprio terminal, de mode que, na vez do jogador, 
+           será mostrada a lista de cartas em sua mão, e quais suas opções de ação(como “comer”, desistir, jogador determinada carta, etc);
+        5) O jogo não possui bots. Dessa maneira, todas as opções discutidas no tópico anterior serão apresentadas no mesmo 
+           terminal e consecutivamente, isto é, logo após um jogador ter feito sua jogada, a vez será dada ao próximo."
+
+        return regras;
+}
+
+void Interface::sair();
+
+void Interface::resetar();
+
+void Interface::comecar();
