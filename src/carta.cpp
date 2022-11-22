cd73e1e8135210c437381637b8e5922d51706f27
Gustavo <gstavomota1@gmail.com>
2022-11-22 20:37:20 -0300

    Carta/CartaEspercial: Implementando biding do C

--- src/carta.cpp
+++ src/carta.cpp
@@ -17,3 +17,15 @@ numero_da_carta Carta::getNumero(){
 	return _numero;
 }
 
+extern "C" {
+	CorDaCarta carta_get_cor(Carta* self) {
+		return self->getCor();
+	};
+	numero_da_carta carta_get_numero(Carta* self) {
+		return self->getNumero();
+	};
+	void carta_delete(Carta* self) {
+		return self->~Carta();
+	};
+}
+
