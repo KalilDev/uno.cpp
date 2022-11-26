5e6f1c39ec297b4822e9d2630f176a893db8b02b
RaphaelLucasAmaral <raphaelamaral1069@hotmail.com>
2022-11-26 18:10:14 -0300

    Jogador: teste: implementação

--- test/jogador.cpp
+++ test/jogador.cpp
@@ -6,8 +6,28 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("foo") {
-    Jogador j10(10); 
-    CHECK_EQ(10, j10.getId());
+TEST_CASE("id do jogador") {
+    Jogador j10(10), j100(100); 
     
+    CHECK_EQ(10, j10.getId());
+    CHECK_EQ(100, j100.getId());
+    
+}
+TEST_CASE("mao do jogador"){
+    auto c = new Carta(CorDaCarta::Vermelho,1);
+    auto c1 = new Carta(CorDaCarta::Amarelo,5);
+    auto c2 = new Carta(CorDaCarta::Azul,6);
+
+    auto jogador = Jogador{20};
+    auto jogador1 = Jogador{30};
+    auto jogador2 = Jogador{40};
+
+    jogador.getMao()->adicionarCarta(c);
+    jogador1.getMao()->adicionarCarta(c1);
+    jogador2.getMao()->adicionarCarta(c2);
+
+    CHECK_EQ(c, jogador.getMao()[0]);
+    CHECK_EQ(c1, jogador1.getMao()[0]);
+    CHECK_EQ(c2, jogador2.getMao()[0]);
+
 }
