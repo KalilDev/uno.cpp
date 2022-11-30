2b7c378c70207864f4b44b554622865ca1ef1e18
Gustavo <gstavomota1@gmail.com>
2022-11-30 18:55:20 -0300

    Teste:Carta/CartaEsp implementando testes

--- test/carta_especial.cpp
+++ test/carta_especial.cpp
@@ -6,5 +6,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("foo") {
+TEST_CASE("Metódos de CartaEspecial") {
+    CartaEspecial cartaEsp(Amarelo, Bloqueia);
+
+    CHECK(cartaEsp.getCor() == 0);
+    CHECK(cartaEsp.getTipo() == 10);
+    CHECK(cartaEsp.toString() == "bloqueia amarelo");
 }
