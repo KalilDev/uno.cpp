f1db59bc1cff3330791f418bcafbe3497c0189e4
Bruno Lima <brunolima280103@gmail.com>
2022-11-30 13:23:45 -0300

    Mao: implementando restante dos testes

--- test/mao.cpp
+++ test/mao.cpp
@@ -12,3 +12,31 @@ TEST_CASE("Tamanho da mao: ") {
     M.adicionarCarta(new Carta(CorDaCarta::Vernelho, 2));
     CHECK(M.size() == 1);
 }
+
+TEST_CASE("Teste begin e and: "){
+    Mao M;
+    CHECK(*M.begin() == M[0]);
+    CHECK(*M.end() == M[0]+1);
+}
+
+TEST_CASE("Verificando a função adicionarCarta: "){
+    Mao M;
+    auto c = new Carta(CorDaCarta::Vermelho, 1);
+    M.adicionarCarta(c);
+    auto c1 = new Carta(CorDaCarta::Azul, 2);
+    M.adicionarCarta(c1);
+    CHECK(c == M[0]);
+    CHECK(c1 == M[1]);
+}
+
+TEST_CASE("Verificando a função removerCarta: "){
+    Mao M;
+    auto c = new Carta(CorDaCarta::Vermelho, 1);
+    M.adicionarCarta(c);
+    M.removeCarta(0);
+    auto c1 = new Carta(CorDaCarta::Azul, 2);
+    M.adicionarCarta(c1);
+    M.removeCarta(1);
+    CHECK(c == M[0]);
+    CHECK(c1 == M[1]);
+}
