96bdc3eefd058a678510e078548aecdb0236d7cf
Bruno Lima <brunolima280103@gmail.com>
2022-11-30 13:18:55 -0300

    Mao: teste para verificar tamanho da mão

--- test/mao.cpp
+++ test/mao.cpp
@@ -6,3 +6,9 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+TEST_CASE("Tamanho da mao: ") {
+    Mao M;
+    CHECK(M.size() == 0);
+    M.adicionarCarta(new Carta(CorDaCarta::Vernelho, 2));
+    CHECK(M.size() == 1);
+}
