5c1048f55a173f45099db36c3a35da8cf4f3b505
KalilDev <kalilgamer0@gmail.com>
2022-12-03 16:45:51 -0300

    util: test: implement

--- test/util.cpp
+++ test/util.cpp
@@ -5,6 +5,22 @@
 #include "util.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include <stdexcept>
+#include <string>
+#include <cstring>
 
-TEST_CASE("foo") {
+class Exception : public std::exception {
+private:
+    std::string _message;
+public:
+    Exception(): _message("Exception") {}
+    const char *what() const noexcept {
+        return _message.c_str();
+    }
+};
+
+TEST_CASE("Teste para c string") {
+    char *erro;
+    exception_to_c(&erro, Exception());
+    CHECK_EQ(0, strcmp(erro, "Exception"));
 }
